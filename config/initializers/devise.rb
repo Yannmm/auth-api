da0210d8106a12a76f8842b7a7f0434a33f1374e
Rayman Yan <ryan@aligntech.com>
2024-07-15 11:12:46 +0800

    make it run

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -313,11 +313,11 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
-    jwt.dispatch_request = [
+    jwt.dispatch_requests = [
       ['POST', %r{^/users/sign_in$}]
 
     ]
-    jtw.revocation_requests = [
+    jwt.revocation_requests = [
       ['DELETE', %r{^/users/sign_out$}]
     ]
     jwt.expiration_time = 120.minutes.to_i
